73ba2aa9a6eeaf5535c4cbb8ecdbcc84a233c3c9
Cickruss <icaro.0.macedo@gmail.com>
2023-03-15 15:56:40 -0300

    Encontrado a chamada de função

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 const puppeteer = require('puppeteer');
 
+async function initBrowser() {
 
-async function Login(params) {
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
+    const browser = await puppeteer.launch({ args: ["--incognito"], headless: false }); //Launches browser in incognito
+    const context = await browser.createIncognitoBrowserContext();
+    const page = await context.newPage(); //Ensures the new page is also incognito
+    await page.evaluateOnNewDocument(() => { delete navigator.proto.webdriver; });
+    await page.goto('http://localhost/Biblivre5/?action=circulation_reservation'); //goes to given link
+    return page;
+}
 
-    await page.goto('http://localhost/Biblivre5/?action=circulation_reservation');
+async function Login(page) {
     await page.type('[name="username"]', 'admin');
     await page.type('[name="password"]', 'abracadabra');
     await page.focus('[name="password"]');
     await page.keyboard.press('Enter');
     await page.waitForNavigation();
-    await page.click('[data-action="circulation_lending"]');
 
+    return page;
 }
 
-const page = Login();
+(async () => {
+    const page = await initBrowser()
+    await Login(page)
+})()
+
+// chamada de função padrão //
+/*(async () => {
+    const page = await initBrowser()
+    await Login(page)
+})() */
+
+
