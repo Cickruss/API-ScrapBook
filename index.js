3de4f8f4e6c6d028614abba42afd48f20e7c08c4
Isadora Santos de Lacerda Oliveira <isadora.oliveira@me.com>
2023-03-16 12:02:35 -0300

    Função de Emprestimo concluida!

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const puppeteer = require('puppeteer');
-
+// INICIAR SISTEMA //
 async function initBrowser() {
 
     const browser = await puppeteer.launch({ args: ["--incognito"], headless: false }); //Launches browser in incognito
@@ -18,31 +18,47 @@ async function Login(page) {
 
     return page;
 }
+///////////////////////////////
+
+
 
 // ABRE A ABA DE EMPRÉSTIMOS //
-async function Lending(page) {
-    await page.waitForNavigation();
+async function LendingPage(page) {
     const menu_circulation = await page.$('li.menu_circulation');
     await menu_circulation.hover();
     await page.waitForSelector('ul.submenu');
     const lending_opt = await menu_circulation.$('a[href="?action=circulation_lending"]');
     await lending_opt.click();
 }
-/*
-async function Lending(page) {
-    await page.click('[href="?action=circulation_reservation"]');
+async function InputNameFromCard(page) {
+    const inputUser = '[placeholder="Preencha os termos da pesquisa"]';
+    await page.type(inputUser,'00002');
+    await page.focus('[placeholder="Preencha os termos da pesquisa"]');
+    await page.keyboard.press('Enter');
 }
-*/
 
+async function InputBookFromRfid(page) {
+    const InputBook = '[placeholder="Tombo patrimonial"]';
+    await page.type(InputBook, 'Bib.2023.1');
+    await page.focus(InputBook);
+    await page.keyboard.press('Enter');
+    await page.waitForTimeout(500);
+    const [lendButton] = await page.$x("//a[contains(text(), 'Emprestar')]");
+    await lendButton.click();
+}
+
+// Tempo de desenvolvimento: 2h20 :)))
+///////////////////////////////////////////
+
+
+// FUNÇÃO PRINCIPAL //
 (async () => {
     const page = await initBrowser()
     await Login(page);
     await page.waitForNavigation();
-    await Lending(page);
+    await LendingPage(page);
+    await page.waitForNavigation();
+    await InputNameFromCard(page);
+    await InputBookFromRfid(page);
 })()
-
-// chamada de função padrão //
-/*(async () => {
-    const page = await initBrowser()
-    await Login(page)
-})() */
+/////////////////////////////////////////
