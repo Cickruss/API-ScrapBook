345d8c2a9f4445d2120f0319c05178697e8a3bad
Cickruss <icaro.0.macedo@gmail.com>
2023-05-04 19:13:08 -0300

    Começo das novas telas com interação com o usuário

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+//const Load = require('./LoanPage');
 
 // INICIAR SISTEMA //
 async function initBrowser() {
@@ -6,8 +7,8 @@ async function initBrowser() {
     const browser = await puppeteer.launch({ args: ["--incognito"], headless: false });
     const context = await browser.createIncognitoBrowserContext();
     const page = await context.newPage();
-    await page.evaluateOnNewDocument(() => { delete navigator.proto.webdriver; });
-    await page.goto('http://127.0.0.1:8080/Biblivre5/'); //goes to given link
+    await page.evaluateOnNewDocument(() => { delete navigator.webdriver; });
+    await page.goto('http://127.0.0.1:8080/Biblivre5/');
     return page;
 }
 
@@ -19,10 +20,8 @@ async function Login(page) {
 
     return page;
 }
-///////////////////////////////
-
-
 // ABRE A ABA DE EMPRÉSTIMOS //
+
 async function LendingPage(page) {
     const menuCirculation = await page.$('li.menu_circulation');
     await menuCirculation.hover();
@@ -30,20 +29,29 @@ async function LendingPage(page) {
     const lendingOption = await menuCirculation.$('a[href="?action=circulation_lending"]');
     await lendingOption.click();
 }
+async function ChangeComboBoxLendingPage(page){
+    const comboTextField = await page.$('div.combo_text');
+    await comboTextField.click();
+    const [observation] = await page.$x("//div[contains(text(), 'Observações')]");
+    await observation.click();
+}
 async function InputNameFromCard(page) {
     const inputUser = '[placeholder="Preencha os termos da pesquisa"]';
-    const userRegistration = '00001';
+    const userRegistration = '1044597020';
     await page.type(inputUser, userRegistration);
     await page.focus(inputUser);
     await page.keyboard.press('Enter');
 }
 
 async function InputBookFromRfid(page) {
-    const InputBook = '[placeholder="Tombo patrimonial"]';
-    let bookRegistration = 'Bib.2023.1';
-    await page.type(InputBook, bookRegistration);
-    await page.focus(InputBook);
-    await page.keyboard.press('Enter');
+    const inputBook = '[placeholder="Tombo patrimonial"]';
+    let bookRegistration = ['1044600329','3816698861'];
+    for (let i = 0; i < bookRegistration.length; i++) {
+        await page.type(inputBook, '');
+        await page.type(inputBook, bookRegistration[i]);
+        await page.focus(inputBook);
+        await page.keyboard.press('Enter');
+    }
 }
 
 async function ClickLendingButton(page) {
@@ -59,10 +67,57 @@ async function ClickGiveBack(page) {
 
 // Tempo de desenvolvimento: 3h :)))
 ///////////////////////////////////////////
+// Cadastro de usuário -> Novo usuário
+// Esperar campo de nome ser preenchido
+// Conferir se o campo de nome está preenchido
+// Se sim, passar para o campo de telefone ou outra coisa
+// Confirmar se o campo de nome está vazio, se sim -> esperar que seja diferente de "" (o admin passar o cartão)
+// Cadastrar depois disso
+
+
+// TELA DE CADASTRO DE USUÁRIOS //
+async function UserRegistration(page){
+    const menuCirculation = await page.$('li.menu_circulation');
+    await menuCirculation.hover();
+    await page.waitForSelector('ul.submenu');
+    const lendingOption = await menuCirculation.$('a[href="?action=circulation_user"]');
+    await lendingOption.click();
+}
+
+// Função que clica no botão de Novo Usuário.
+async function ClickNewUser(page){
+    const newUserButton = ".new_record_button";
+    await page.click(newUserButton);
+}
+
+async function EnterUserName(page){
+    await page.waitForSelector('[name="name"]')
+    const usernameField = '[name="name"]';
+    //const usernameField = await page.$('input[name="name"]');
+    //await page.focus(usernameField);
+    const userName = 'Bruninha Gameplay';
+    async function EnterUserType(page) {
+        const userTypeField = '[name="type"]';
+        const type = '1';
+        await page.select(userTypeField, type);
+    }
+    async function EnterUserEmail(page) {
+        const userEmail = "porcoassado@yahoo.com";
+        const userEmailField = '[name="email"]';
+        await page.type(userEmailField, userEmail);
+    }
+    async function EnterRfidId(page) {
+    await page.type(usernameField, userName); //ao inves de nome do leitor será o nome do formulário.
+}
+    const rfidId = "3815190397";
+    const obsField = '[name="obs"]'
+    await page.type(obsField, rfidId);
+}
+// Tempo de desenvolvimento: 1:24
 
 
 // FUNÇÃO PRINCIPAL //
-/*(async () => {
+(async () => {
     const page = await initBrowser()
     await Login(page);
 
@@ -70,18 +125,34 @@ async function ClickGiveBack(page) {
     await page.waitForNavigation();
     await LendingPage(page);
     await page.waitForNavigation();
+    await ChangeComboBoxLendingPage(page);
     await InputNameFromCard(page);
     await InputBookFromRfid(page);
     await ClickLendingButton(page);
     /////////////////////
 
-    // Devolução de livro //
+    /*// Devolução de livro //
     await page.waitForNavigation();
     await LendingPage(page);
     await page.waitForNavigation();
     await InputNameFromCard(page);
     await InputBookFromRfid(page);
     await ClickGiveBack(page);
-    ///////////////////////
-})()*/
+    ///////////////////////*/
+
+
+    /*// Cadastro de usuários //
+    await page.waitForNavigation();
+    await UserRegistration(page);
+    await page.waitForNavigation();
+    await ClickNewUser(page);
+    await page.waitForNavigation();
+    await EnterUserName(page);
+    await EnterUserType(page);
+    await EnterUserEmail(page);
+    await EnterRfidId(page);*!/*/
+
+
+
+})()
 /////////////////////////////////////////
